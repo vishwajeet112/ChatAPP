09682951a590f6ae51a3384f579692e3af8e0c8a
Vishwajeet <81477266+vishwajeet112@users.noreply.github.com>
2023-02-26 16:13:51 +0530

    c

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,68 @@
+import 'package:chatapp_firebase/pages/group_info.dart';
+import 'package:chatapp_firebase/service/database_service.dart';
+import 'package:chatapp_firebase/widgets/widgetsa.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  final String userName;
+  final String groupName;
+  final String groupId;
+  const ChatPage(
+      {Key? key,
+      required this.groupId,
+      required this.groupName,
+      required this.userName})
+      : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  Stream<QuerySnapshot>? chats;
+  String admin = "";
+
+  @override
+  void initState() {
+    getChatandAdmin();
+    super.initState();
+  }
+
+  getChatandAdmin() {
+    DatabaseService().getChats(widget.groupId).then((val) {
+      setState(() {
+        chats = val;
+      });
+    });
+    DatabaseService().getGroupAdmin(widget.groupName).then((val) {
+      setState(() {
+        admin = val;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text(widget.groupName),
+        backgroundColor: Theme.of(context).primaryColor,
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(
+                    context,
+                    GroupInfo(
+                        groupId: widget.groupId,
+                        groupName: widget.groupName,
+                        adminName: admin));
+              },
+              icon: const Icon(Icons.info))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/service/database_service.dart
@@ -0,0 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String? uid;
+  DatabaseService({this.uid});
+
+  //refernce for our collections
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection("users");
+  final CollectionReference groupCollection =
+      FirebaseFirestore.instance.collection("groups");
+
+  //saving user data
+  Future savingUserData(String fullName, String email) async {
+    return await userCollection.doc(uid).set({
+      "fullName": fullName,
+      "email": email,
+      "groups": [],
+      "profilePic": "",
+      "uid": uid,
+    });
+  }
+
+  //getting user data
+  Future gettingUserData(String email) async {
+    QuerySnapshot snapshot =
+        await userCollection.where("email", isEqualTo: email).get();
+    return snapshot;
+  }
+
+  //getting the usergroups
+  getUserGroups() async {
+    return userCollection.doc(uid).snapshots();
+  }
+
+  //creating a group
+  Future createGroup(String userName, String id, String groupName) async {
+    DocumentReference groupDocumentReference = await groupCollection.add({
+      "groupName": groupName,
+      "groupIcon": "",
+      "admin": "${id}_$userName",
+      "members": [],
+      "groupId": "",
+      "recentMessage": "",
+      "recentMessageSender": "",
+    });
+
+    //update members
+    await groupDocumentReference.update({
+      "members": FieldValue.arrayUnion(["${uid}_$userName"]),
+      "groupId": groupDocumentReference.id,
+    });
+
+    DocumentReference userDocumentReference = await userCollection.doc(uid);
+    return await userDocumentReference.update({
+      "groups":
+          FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"]),
+    });
+  }
+
+  //getting the chats
+  getChats(String groupId) async {
+    return groupCollection
+        .doc(groupId)
+        .collection("messages")
+        .orderBy("time")
+        .snapshots();
+  }
+
+  Future getGroupAdmin(String groupId) async {
+    DocumentReference d = groupCollection.doc(groupId);
+    DocumentSnapshot documentSnapshot = await d.get();
+    return documentSnapshot['admin'];
+  }
+
+  //get group members
+  getGroupMembers(groupId) async {
+    return groupCollection.doc(groupId).snapshots();
+  }
+}
